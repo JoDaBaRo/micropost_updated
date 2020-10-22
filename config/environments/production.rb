7eaf550709d87f02b2b9d67dd737c99ca20b86df
cristianleonardop <cristianleonardo999@gmail.com>
2020-10-22 12:45:27 -0500

    feat: config send emails with gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,14 +81,15 @@ Rails.application.configure do
   host = 'https://dry-crag-63247.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   config.action_controller.default_url_options = {host: host}
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: ENV['gmail_username'],
+    password: ENV['gmail_password'],
+    authentication: 'plain',
+    enable_starttls_auto: true
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
